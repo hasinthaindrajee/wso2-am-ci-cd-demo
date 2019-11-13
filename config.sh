0abce21931cfa083be81c63df9d098da702e21d7
Hasintha Indrajee <hasintha@wso2.com>
2019-11-13 02:03:32 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo $(which apimcli)
-/Users/hasinthaindrajee/Downloads/apimcli/apimcli version
+bash /Users/hasinthaindrajee/Downloads/apimcli/apimcli version
 
 rm -rf $HOME/.wso2apimcli
 
 echo 'setting up test environment'
-/Users/hasinthaindrajee/Downloads/apimcli/apimcli add-env -n test \
+bash /Users/hasinthaindrajee/Downloads/apimcli/apimcli add-env -n test \
                     --registration https://localhost:9444/client-registration/v0.14/register \
                     --apim https://localhost:9444 \
                     --token https://localhost:8244/token \
@@ -15,10 +15,10 @@ echo 'setting up test environment'
                     --api_list https://localhost:9444/api/am/publisher/v0.14/apis \
                     --app_list https://localhost:9444/api/am/store/v0.14/applications
 echo 'logging into test'
-/Users/hasinthaindrajee/Downloads/apimcli/apimcli login test -u $USERNAME -p $PASSWORD -k
+bash /Users/hasinthaindrajee/Downloads/apimcli/apimcli login test -u $USERNAME -p $PASSWORD -k
 
 echo 'setting up prod environment'
-/Users/hasinthaindrajee/Downloads/apimcli/apimcli add-env -n prod \
+bash /Users/hasinthaindrajee/Downloads/apimcli/apimcli add-env -n prod \
                     --registration https://localhost:9445/client-registration/v0.14/register \
                     --apim https://localhost:9445 \
                     --token https://localhost:8245/token \
@@ -27,4 +27,4 @@ echo 'setting up prod environment'
                     --api_list https://localhost:9445/api/am/publisher/v0.14/apis \
                     --app_list https://localhost:9445/api/am/store/v0.14/applications
 echo 'logging into prod'
-/Users/hasinthaindrajee/Downloads/apimcli/apimcli login prod -u $USERNAME -p $PASSWORD -k
+bash /Users/hasinthaindrajee/Downloads/apimcli/apimcli login prod -u $USERNAME -p $PASSWORD -k
